f1937e41ed757b2835ad51edd009407437bf4ed8
mcakeyerima <mcakyerima@gmail.com>
2022-09-05 14:27:44 +0100

    Font-Size-Fix

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -23,7 +23,7 @@ function Banner({ netflixOriginals } : Props) {
     
 
   return (
-    <div className="flex flex-col space-y-2 py-16  md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+    <div className="flex flex-col space-y-2 py-16  md:space-y-4 lg:h-[69vh] lg:justify-end lg:pb-12 ">
         <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10">
             <Image 
                 src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
@@ -31,19 +31,22 @@ function Banner({ netflixOriginals } : Props) {
                 objectFit='cover'
                 />
         </div>
-        <h1 className="text-2xl font-bold md:text-4xl lg:text-6xl mt-12">{movie?.title || movie?.name || movie?.original_name}</h1>
-        <p className="max-w-xs text-xs text-shadow-md md:max-w-lg mg:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
+        <div className="absolute  mt-32 space-y-4">
+            <h1 className="text-2xl font-bold md:text-4xl lg:text-6xl mt-12">{movie?.title || movie?.name || movie?.original_name}</h1>
+            <p className="max-w-xs text-xs text-shadow-md md:max-w-lg mg:text-lg lg:max-w-2xl lg:text-xl">{movie?.overview}</p>
 
-        <div className="flex gap-3">
-            <button className="bannerButton bg-white text-black">
-                <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/>
-                Play
-                
-            </button>
-            <button className="bannerButton bg-[gray]/70">
-                More Info
-                <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/>    
-            </button>
+            <div className="flex gap-3">
+                <button className="bannerButton bg-white text-black">
+                    <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/>
+                    Play
+                    
+                </button>
+                <button className="bannerButton bg-[gray]/70">
+                    More Info
+                    <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/>    
+                </button>
+        </div>
+      
         </div>
     </div>
   )
